85c724351827b7ea744414956cd0be502f5a8926
yuzeBai <1600242974@qq.com>
2022-11-16 08:16:58 +0000

    Finish frequency counter.

--- frequencies.py
+++ frequencies.py
@@ -4,4 +4,15 @@
 def frequencies(items):
     frequencies = {}
     # Your code goes here
+    list = []
+    for x in range(len(items)):
+          items[x] = str(items[x]) 
+       
+    for i in items:
+    
+     if i not in list:
+          a = items.count(i)
+          frequencies[str(i)]= a
+          list.append(i)
+
     return frequencies
